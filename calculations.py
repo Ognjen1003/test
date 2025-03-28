43c0e77ca658600d2254b33f6d63172b24363b44
ogmiosram <ogmiosram@yahoo.com>
2025-03-28 17:37:39 +0100

    logging malo popravio i tak

--- calculations.py
+++ calculations.py
@@ -2,27 +2,35 @@ import numpy as np
 import math
 import pandas as pd
 import warnings
-import os
 import datetime
-#from flow_functions import dp_table
 from numpy.ma.core import log10
 from Classes.calc import Calc 
 warnings.filterwarnings("ignore")
 
 def calculate_steps(steps: int, length: int, d_in: float, e: float, p: float, tK: float, qm: float, case: str) -> float:
-    
-    ################################### case handling #################################
-    url = f"https://raw.githubusercontent.com/dvulin/lookup/main/lookup_table_{case}.csv"
 
-    """
-    load data from lookup table:
-    t (°C), p (bar), FL (liquid fraction), Fv (vapour fraction), rho_L (kg/m3), rho_g (kg/m3),
-    mu_L (mPas), mu_g (mPas), Z_oil (compressibility factor), Z_gas (compressibility factor)
-    """
-    df_lookup = pd.read_csv(url)
-    df_lookup['mu_L']=df_lookup['mu_L']*0.001
-    df_lookup['mu_g']=df_lookup['mu_g']*0.001
-    print (f'loaded case: {case}')
+    ################################### case handling #################################
+    check_case(case)
+    df_lookup = None
+    is_pure_CO2 = False
+    if case.upper()=='CO2':
+        is_pure_CO2 = True
+    else:
+        #TODO: DB
+        url = f"https://raw.githubusercontent.com/dvulin/lookup/main/lookup_table_{case}.csv"
+        """
+        load data from lookup table:
+        t (°C), p (bar), FL (liquid fraction), Fv (vapour fraction), rho_L (kg/m3), rho_g (kg/m3),
+        mu_L (mPas), mu_g (mPas), Z_oil (compressibility factor), Z_gas (compressibility factor)
+        """
+        try:
+            df_lookup = pd.read_csv(url)
+            df_lookup['mu_L']=df_lookup['mu_L']*0.001
+            df_lookup['mu_g']=df_lookup['mu_g']*0.001
+            print (f'loaded case: {case}')
+        except Exception as e:
+            print(f"Error loading lookup table: {e}")
+            raise e
     ###################################################################################        
 
     dfi = pd.DataFrame(columns=['L', 'p1', 't', 'mu', 'rho_g', 'u', 'Re', 'ff', 'dp', 'p2'])
@@ -30,8 +38,6 @@ def calculate_steps(steps: int, length: int, d_in: float, e: float, p: float, tK
     p_out = 0.0                         #moja varijabla
     area = 0.25*np.pi*d_in**2
     
-    Dexp = 0.315925 
-
     parameter_sensitivity = {}
     d_in_sens = {}
     e_sens = {}      
@@ -45,7 +51,10 @@ def calculate_steps(steps: int, length: int, d_in: float, e: float, p: float, tK
 
 
     dfi = pd.DataFrame(columns=['L', 'p1', 't', 'mu', 'rho_g', 'u', 'Re', 'ff', 'dp', 'p2'])
-    dfi = calc_instance.dp_table(lookup_table = df_lookup, L = length, A = area, d_in = d_in, e = e, p1 = p, T1=tK, qm = qm, nsteps = steps)
+
+    dfi = calc_instance.dp_table_combined(L = length, A = area, d_in = d_in, 
+                    e = e, p1 = p, T1 = tK, qm = qm, is_pure_CO2 = is_pure_CO2, nsteps = steps, lookup_table = df_lookup)
+
                         
     dfi['rho_g'] = dfi['rho_g'].astype(float)         # format for plotting
     dfi['mu'] = dfi['mu'].astype(float)               # format for plotting
@@ -69,3 +78,8 @@ def calculate_steps(steps: int, length: int, d_in: float, e: float, p: float, tK
     print(f'end {datetime.datetime.now().strftime(TIMEFORMAT)}') 
     
     return p_out
+
+def check_case(case: str):
+    valid_cases = ["case1", "case2", "case3", "CO2"]
+    if case not in valid_cases:
+        raise ValueError(f"Invalid case: {case}. Must be one of {valid_cases}")
