0b9ad0f82f57e1773d15505dc31130d477359bb7
ogmiosram <ogmiosram@yahoo.com>
2025-04-01 02:39:20 +0200

    static cache

--- test.py
+++ test.py
@@ -4,10 +4,6 @@ import datetime
 url = "http://127.0.0.1:8000/calculate"
 #url = "https://test-production-f873.up.railway.app/calculate"
 
-
-#namjesteni parametri da se covjek ne muci sa POST-o ako ne mora (40,40000) kao dole
-# url = "https://test-production-f873.up.railway.app/calculateBlank"
-
 begin = datetime.datetime.now()
 
 input_data = {
@@ -18,15 +14,17 @@ input_data = {
     "p": 4000000,
     "T": 293.15,
     "qm": 23.75,
-    "case": "case1"   
+    "case": "case1",   
+    "visual": 1
 }
 
 response = requests.post(url, json=input_data)
 
 if response.status_code == 200:
-    result = response.json()
+    #result = response.json()
+    result = response.text
     end = datetime.datetime.now()
     runtime = end - begin
-    print(f"p2: {result}  in {runtime} seconds")
+    print(f"{result}  in {runtime} seconds")
 else:
     print(f"Greska: {response.status_code}, {response.text}")
